60c996cffbb93768fe29ca2fd4996fd05edb3277
Sam Brez <sambreslin02@gmail.com>
2024-01-23 18:46:21 -0500

    updated to add book functionality

--- public/js/addBook.js
+++ public/js/addBook.js
@@ -2,12 +2,16 @@ async function addBook() {
     const title = document.querySelector('title').value;
     const author = document.querySelector('author').value;
     const genre = document.querySelector('genre').value;
+    const recommendation = document.querySelector('rec').value;
+    const review = document.querySelector('review').value;
     const userId = document.querySelector('userId').value; // This may need to be pulled innately from session?
   
     const data = {
       title: title,
       author: author,
       genre: genre,
+      recommendation: recommendation,
+      review: review,
       user_id: userId
     };
   
