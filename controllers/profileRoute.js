1a8c5fa7af57f326e41646fce011b2cb40dd075d
Sam Brez <sambreslin02@gmail.com>
2024-01-22 19:29:59 -0500

    bug fixes to /profile route

--- controllers/profileRoute.js
+++ controllers/profileRoute.js
@@ -3,38 +3,22 @@ const { Books, User, Reviews } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
+    
     try {
-        const userBooks = await Books.findAll({
-            where: {
-                user_id: req.session.user_id
-            },
-            include: [
-                {
-                    model: Reviews,
-                    attributes: ['reviewTitle', 'review', 'date_created', 'recommendation', 'user_id'],
-                    include: [
-                        {
-                            model: User,
-                            attributes: ['name'],
-                            where: {
-                                user_id: user_id
-                            }
-                        }
-                    ]
-                }
-            ]
+        const userData = await User.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Books }],
         });
-
-        const books = userBooks.map((book) => book.get({ plain: true }));
-
-        res.render("profile", {
-            books,
-            logged_in: true
+    
+        const user = userData.get({ plain: true });
+    
+        res.render('profile', {
+          ...user,
+          logged_in: true
         });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;
