42af8c559c965087486e5b38debae98f60977afe
michael <mikek222@gmail.com>
2024-01-24 04:01:36 +0300

    added wishlist model, routing, and associations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./UserRoutes');
 const bookRoutes = require('./booksRoute');
 const reviewRoutes = require('./reviewsRoute');
+const wishlistRoutes = require('./wishlistRoute')
 
 router.use('/users', userRoutes);
 router.use('/books', bookRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/wishlist', wishlistRoutes);
 
 
 module.exports = router;
