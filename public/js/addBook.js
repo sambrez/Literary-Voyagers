e9e52e8c994cb7c49decfcfe2915b1f710123517
Sam Brez <sambreslin02@gmail.com>
2024-01-25 19:49:59 -0500

    add review functionality, bug fixes

--- public/js/addBook.js
+++ public/js/addBook.js
@@ -1,42 +1,44 @@
 const addBook = async (event) => {
-    const title = document.querySelector('title').value;
-    const author = document.querySelector('author').value;
-    const genre = document.querySelector('genre').value;
-    const recommendation = document.querySelector('rec').value;
-    const review = document.querySelector('review').value;
-    const userId = req.sessions.user_id
-  
-    const data = {
-      title: title,
-      author: author,
-      genre: genre,
-      recommendation: recommendation,
-      review: review,
-      user_id: userId
-    };
-  
-    try {
-      const response = await fetch('/api/books/add_book', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to add book');
-      }
-  
-      const newBook = await response.json();
-      console.log('Book added successfully:', newBook);
-  
-      document.location.replace('/profile');
-    } catch (error) {
-      console.error('Error adding book:', error.message);
-    }
+  event.preventDefault();
+
+  const title = document.querySelector('title').value;
+  const author = document.querySelector('author').value;
+  const genre = document.querySelector('genre').value;
+  const recommendation = document.querySelector('rec').value;
+  const review = document.querySelector('review').value;
+  const userId = req.sessions.user_id
+
+  const data = {
+    title: title,
+    author: author,
+    genre: genre,
+    recommendation: recommendation,
+    review: review,
+    user_id: userId
   };
 
-  document
-    .querySelector('#book-form')
-    .addEventListener('submit', addBook);
+  try {
+    const response = await fetch('/api/books/add_book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add book');
+    }
+
+    const newBook = await response.json();
+    console.log('Book added successfully:', newBook);
+
+    document.location.replace('/profile');
+  } catch (error) {
+    console.error('Error adding book:', error.message);
+  }
+};
+
+document
+  .querySelector('#book-form')
+  .addEventListener('submit', addBook);
--- public/js/review.js
+++ public/js/review.js
@@ -1,15 +1,17 @@
 const newReview = async (event) => {
     event.preventDefault();
   
-    const header = document.querySelector('#review-header').value.trim();
-    const body = document.querySelector('#review-body').value.trim();
-    const recommendation = document.querySelector('#boolean').value.trim();
+    const recommendation = document.querySelector('#recommendation').value.trim();
+    const review = document.querySelector('#review').value.trim();
+    const book_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+    
   
-    if (header && body && recommendation) {
+    if (recommendation && review) {
       const response = await fetch(`/api/reviews`, {
         method: 'POST',
-        // recommendation is boolean value
-        body: JSON.stringify({ header, body, recommendation }),
+        body: JSON.stringify({recommendation, review, book_id}),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -23,26 +25,26 @@ const newReview = async (event) => {
     }
   };
   
-  const deleteReview = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+  // const deleteReview = async (event) => {
+  //   if (event.target.hasAttribute('data-id')) {
+  //     const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/reviews/${id}`, {
-        method: 'DELETE',
-      });
+  //     const response = await fetch(`/api/reviews/${id}`, {
+  //       method: 'DELETE',
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to delete review');
-      }
-    }
-  };
+  //     if (response.ok) {
+  //       document.location.replace('/');
+  //     } else {
+  //       alert('Failed to delete review');
+  //     }
+  //   }
+  // };
 
   document
   .querySelector('.new-review')
   .addEventListener('submit', newReview);
 
-  document
-  .querySelector('.delete-review')
-  .addEventListener('click', deleteReview);
+  // document
+  // .querySelector('.delete-review')
+  // .addEventListener('click', deleteReview);
