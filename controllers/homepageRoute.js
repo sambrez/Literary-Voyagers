61ef140be32229e71042ea235b51b529faeca07d
michael <mikek222@gmail.com>
2024-01-29 19:04:07 +0300

    wishlist functionality now working, modals added for both pass and fail states of adding new books to the wishlist

--- controllers/homepageRoute.js
+++ controllers/homepageRoute.js
@@ -60,11 +60,11 @@ router.get("/books/:id", withAuth, async (req, res) => {
 
 router.get('/wishlist', withAuth, async (req, res) => {
   try {
-    const userId = req.session.user_id;
+
 
     // Retrieve the user's wishlist including associated books
     const userWishlist = await Wishlist.findAll({
-      where: { userId },
+      where: { userId: req.session.user_id },
       include: [
         {
           model: Books,
@@ -85,7 +85,6 @@ router.get('/wishlist', withAuth, async (req, res) => {
   }
 });
 
-module.exports = router;
 
 // for login and sign up
 router.get("/login", (req, res) => {
